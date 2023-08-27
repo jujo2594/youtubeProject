9266f82334cef557c5875013a44abd68f35b18f4
jujo2594 <105889688+jujo2594@users.noreply.github.com>
2023-08-26 19:11:57 -0500

    Interactivity for the sidebar

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('youtubeProject');
+/* Function and event use to reduce the width of the sidebar*/
+
+let menuIcon = document.querySelector('.menu');
+let sideBar = document.querySelector('.sideBar');
+
+function ocultar(){
+    sideBar.classList.toggle("reduceSideBar");
+}
+
+menuIcon.addEventListener('click', ocultar)
