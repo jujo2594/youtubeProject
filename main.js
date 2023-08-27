23928d54b43572f7710131f97d32c644ddc2a4df
jujo2594 <105889688+jujo2594@users.noreply.github.com>
2023-08-26 22:57:23 -0500

    Fixing hover in sidemenu

--- main.js
+++ main.js
@@ -2,9 +2,25 @@
 
 let menuIcon = document.querySelector('.menu');
 let sideBar = document.querySelector('.sideBar');
+let largeContainer = document.querySelector('.mainContent');
+let largeBanner = document.querySelector('.bannerContainer');
+
+//*! It's not reomving the hover when the menu is reduced
+let shortcuts = document.querySelectorAll('.simdeMenu');
+
 
 function ocultar(){
     sideBar.classList.toggle("reduceSideBar");
+    largeContainer.classList.toggle("bigContent");
+    largeBanner.classList.toggle("largeBannerContainer");
 }
 
-menuIcon.addEventListener('click', ocultar)
+//*! It's not reomving the hover when the menu is reduced
+function hover(){
+    shortcuts.style.transition = "none";
+}
+
+menuIcon.addEventListener('click', ocultar);
+
+//*! It's not reomving the hover when the menu is reduced
+shortcuts.addEventListener('mouseenter', hover)  
