1c7c14293b5f66d03de5c1c2eb39556d547bef1f
jujo2594 <105889688+jujo2594@users.noreply.github.com>
2023-08-28 19:06:50 -0500

    Insert video info in video player

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@
 
 const banner = "js/banner";
 
-const obtenerBanner = async() =>{
+const getBanner = async() =>{
 	try{
 		let petition = await fetch(`${banner}.json`);
 		let response = await petition.json();
@@ -18,13 +18,13 @@ const obtenerBanner = async() =>{
 	}
 }
 
-obtenerBanner();
+getBanner();
 
 /* Using the informacion of Channel Videos from the website, I'll insert the videos visualizations */
 
 const thumbnailsInfo = "js/creativeCodeVideo"
 
-const ObtenerVideos = async() =>{
+const getVideos = async() =>{
 	try{
 		let petition = await fetch(`${thumbnailsInfo}.json`);
 		let response = await petition.json();
@@ -50,4 +50,37 @@ const ObtenerVideos = async() =>{
 	}
 }
 
-ObtenerVideos();
+getVideos();
+
+/* Function that send to the correct video if I click on the thumbnail, and add the videos informacion on the description.*/
+
+
+const putVideos = async()=>{
+	try{
+		let petition = await fetch(`${thumbnailsInfo}.json`);
+		let petition2 = await fetch(`${banner}.json`)
+		let response = await petition.json();
+		let response2 = await petition2.json();
+		let selection = document.querySelector('.play-video');
+		selection.insertAdjacentHTML('beforeend',/* HTML */`
+			${response.contents.map((value)=>/* HTML */`
+			<iframe width="1000" height="600" src="https://www.youtube.com/embed/${}" title="M I L E S D A V I S - Kind Of Blue - Full Album" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			<h3>Web Api NetCore Part22</h3>
+			<div class="playVideoInfo">
+				<p>145k &bull; 3 weeks ago</p>
+				<div>
+					<a href=""><img src="images/like.png" alt="likeIcon">26 k</a>
+					<a href=""><img src="images/dislike.png" alt="likeIcon"></a>
+					<a href=""><img src="images/share.png" alt="likeIcon">Share</a>
+					<a href=""><img src="images/save.png" alt="likeIcon">Save</a>
+					<a class="downloads" ><i class='bx bx-download'></i>Download</a>
+				</div>
+			</div>
+			`)}
+		
+		`)
+	}catch(error){
+		console.error('System Error', error);
+	}
+	
+}
